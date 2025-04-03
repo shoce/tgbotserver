6118816d63580be3ad80c7f7fd8c14c01e50ceeb
shoce <yogze@icloud.com>
2025-04-03 20:24:06 +0530

    dagger

--- .dagger/main.go
+++ .dagger/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"dagger/tgbotserver/internal/dagger"
-
-	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -22,62 +19,35 @@ const (
 )
 
 var (
-	Platforms = []dagger.Platform{
-		"linux/arm64",
-		"linux/amd64",
-	}
-
 	Ctx = context.TODO()
 )
 
 type Tgbotserver struct{}
 
-func (m *Tgbotserver) Build() []*dagger.Container {
+func (m *Tgbotserver) Build() *dagger.Container {
 
 	srcgit := dag.Git(SourceGitUrl).Head().Tree()
 
-	c := make([]*dagger.Container, 0, len(Platforms))
+	a := dag.Container().
+		From(AlpineDockerImage).
+		WithExec([]string{"apk", "upgrade", "--no-cache"}).
+		WithExec([]string{"apk", "add", "--no-cache", "alpine-sdk", "linux-headers", "zlib-dev", "openssl-dev", "gperf", "cmake"}).
+		WithDirectory("/root/tgbotserver/", srcgit).
+		WithDirectory("/root/tgbotserver/build/", dag.Directory()).
+		WithWorkdir("/root/tgbotserver/build/").
+		WithExec([]string{"cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX:PATH=/root/tgbotserver/", "/root/tgbotserver/"}).
+		WithExec([]string{"cmake", "--build", ".", "--target", "install"})
 
-	eg, _ := errgroup.WithContext(Ctx)
+	b := dag.Container().
+		From(AlpineDockerImage).
+		WithExec([]string{"apk", "upgrade", "--no-cache"}).
+		WithExec([]string{"apk", "add", "--no-cache", "openssl", "zlib", "libstdc++"}).
+		WithFile("/bin/tgbotserver", a.File("/root/tgbotserver/bin/telegram-bot-api")).
+		WithWorkdir("/root/").
+		WithEntrypoint([]string{"/bin/tgbotserver", fmt.Sprintf("--http-port=%d", ExposedPort), "--local"}).
+		WithExposedPort(ExposedPort)
 
-	for _, platform := range Platforms {
-
-		eg.Go(func() (err error) {
-
-			fmt.Printf("platform==%s"+NL, platform)
-
-			arch := strings.Split(string(platform), "/")[1]
-			fmt.Printf("arch==%s"+NL, arch)
-
-			a := dag.Container(dagger.ContainerOpts{Platform: platform}).
-				From(AlpineDockerImage).
-				WithExec([]string{"apk", "upgrade", "--no-cache"}).
-				WithExec([]string{"apk", "add", "--no-cache", "alpine-sdk", "linux-headers", "zlib-dev", "openssl-dev", "gperf", "cmake"}).
-				WithDirectory("/root/tgbotserver/", srcgit).
-				WithDirectory("/root/tgbotserver/build/", dag.Directory()).
-				WithWorkdir("/root/tgbotserver/build/").
-				WithExec([]string{"cmake", "-DCMAKE_SYSTEM_PROCESSOR=" + arch, "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX:PATH=/root/tgbotserver/", "/root/tgbotserver/"}).
-				WithExec([]string{"cmake", "--build", ".", "--target", "install"})
-
-			b := dag.Container(dagger.ContainerOpts{Platform: platform}).
-				From(AlpineDockerImage).
-				WithExec([]string{"apk", "upgrade", "--no-cache"}).
-				WithExec([]string{"apk", "add", "--no-cache", "openssl", "zlib", "libstdc++"}).
-				WithFile("/bin/tgbotserver", a.File("/root/tgbotserver/bin/telegram-bot-api")).
-				WithWorkdir("/root/").
-				WithEntrypoint([]string{"/bin/tgbotserver", fmt.Sprintf("--http-port=%d", ExposedPort), "--local"}).
-				WithExposedPort(ExposedPort)
-
-			c = append(c, b)
-
-			return err
-		})
-
-		eg.Wait()
-
-	}
-
-	return c
+	return b
 
 }
 
@@ -91,15 +61,13 @@ func (m *Tgbotserver) Publish(
 	image string,
 ) string {
 
-	d := dag.Container()
+	d := m.Build()
 	if username != "" {
 		d = d.WithRegistryAuth(registry, username, password)
 	}
-	p, _ := d.Publish(Ctx,
+	p, _ := d.Publish(
+		Ctx,
 		registry+"/"+image,
-		dagger.ContainerPublishOpts{
-			PlatformVariants: m.Build(),
-		},
 	)
 
 	return p
