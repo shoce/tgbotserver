98db7ca3c72e6d8a16e7db9cf754b5be82418ac0
shoce <yogze@icloud.com>
2025-10-06 13:15:32 +0530

    /tgbotserver/

--- .dagger/main.go
+++ .dagger/main.go
@@ -11,7 +11,7 @@ const (
 	SourceGitUrl = "https://github.com/tdlib/telegram-bot-api"
 
 	// https://hub.docker.com/_/alpine/tags/
-	AlpineDockerImage = "alpine:3.21.3"
+	AlpineDockerImage = "alpine:3.22"
 
 	ExposedPort = 80
 
@@ -32,19 +32,20 @@ func (m *Tgbotserver) Build() *dagger.Container {
 		From(AlpineDockerImage).
 		WithExec([]string{"apk", "upgrade", "--no-cache"}).
 		WithExec([]string{"apk", "add", "--no-cache", "alpine-sdk", "linux-headers", "zlib-dev", "openssl-dev", "gperf", "cmake"}).
-		WithDirectory("/root/tgbotserver/", srcgit).
-		WithDirectory("/root/tgbotserver/build/", dag.Directory()).
-		WithWorkdir("/root/tgbotserver/build/").
-		WithExec([]string{"cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX:PATH=/root/tgbotserver/", "/root/tgbotserver/"}).
+		WithDirectory("/tgbotserver/", srcgit).
+		WithDirectory("/tgbotserver/build/", dag.Directory()).
+		WithWorkdir("/tgbotserver/build/").
+		WithExec([]string{"cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX:PATH=/tgbotserver/", "/tgbotserver/"}).
 		WithExec([]string{"cmake", "--build", ".", "--target", "install"})
 
 	b := dag.Container().
 		From(AlpineDockerImage).
 		WithExec([]string{"apk", "upgrade", "--no-cache"}).
 		WithExec([]string{"apk", "add", "--no-cache", "openssl", "zlib", "libstdc++"}).
-		WithFile("/bin/tgbotserver", a.File("/root/tgbotserver/bin/telegram-bot-api")).
-		WithWorkdir("/root/").
-		WithEntrypoint([]string{"/bin/tgbotserver", fmt.Sprintf("--http-port=%d", ExposedPort), "--local"}).
+		WithDirectory("/tgbotserver/", dag.Directory()).
+		WithWorkdir("/tgbotserver/").
+		WithFile("/tgbotserver/tgbotserver", a.File("/tgbotserver/bin/telegram-bot-api")).
+		WithEntrypoint([]string{"/tgbotserver", fmt.Sprintf("--http-port=%d", ExposedPort), "--local"}).
 		WithExposedPort(ExposedPort)
 
 	return b
--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,32 @@
 
 
 # https://hub.docker.com/_/alpine/tags
-FROM alpine:3.21.3 AS build
+FROM alpine:3.22 AS build
 ARG TARGETARCH
-WORKDIR /root/
 
 RUN apk upgrade --no-cache
 RUN apk add --no-cache alpine-sdk linux-headers git zlib-dev openssl-dev gperf cmake
 
-RUN git clone --recursive https://github.com/tdlib/telegram-bot-api.git /root/tgbotserver
-RUN cd /root/tgbotserver
-RUN ls -l -a /root/tgbotserver/ /root/tgbotserver/*/
+RUN git clone --recursive https://github.com/tdlib/telegram-bot-api.git /tgbotserver
+RUN ls -l -a /tgbotserver/ /tgbotserver/*/
 
-RUN rm -r -f /root/tgbotserver/build && mkdir /root/tgbotserver/build && cd /root/tgbotserver/build
-RUN cmake -DCMAKE_SYSTEM_PROCESSOR=$TARGETARCH -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/root/tgbotserver/ /root/tgbotserver/
+RUN rm -r -f /tgbotserver/build && mkdir /tgbotserver/build
+WORKDIR /tgbotserver/build/
+RUN cmake -DCMAKE_SYSTEM_PROCESSOR=$TARGETARCH -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=/tgbotserver/ /tgbotserver/
 RUN cmake --build . --target install
 
-RUN ls -l -a /root/tgbotserver/*/
+RUN ls -l -a /tgbotserver/*/
 
 
 
 # https://hub.docker.com/_/alpine/tags
-FROM alpine:3.21.3
+FROM alpine:3.22
 RUN apk upgrade --no-cache
 RUN apk add --no-cache openssl zlib libstdc++
-COPY --from=build /root/tgbotserver/bin/telegram-bot-api /bin/tgbotserver
-RUN ls -l -a /bin/tgbotserver
-WORKDIR /root/
-ENTRYPOINT ["/bin/tgbotserver", "--http-port=80", "--local"]
+RUN mkdir /tgbotserver/
+WORKDIR /tgbotserver/
+COPY --from=build /tgbotserver/bin/telegram-bot-api /tgbotserver/tgbotserver
+RUN ls -l -a /tgbotserver/tgbotserver
+ENTRYPOINT ["/tgbotserver/tgbotserver", "--http-port=80", "--local"]
 
 
